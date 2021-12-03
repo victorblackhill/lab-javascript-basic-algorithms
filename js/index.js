b7af74d7b9cb0eb46d67fb888b5eba0a46f79b10
Hernan Menajovsky <hmenajovsky@gmail.com>
2021-12-03 21:45:13 +0100

    added end solved bonus 2

--- js/index.js
+++ js/index.js
@@ -1,11 +1,11 @@
 // Iteration 1: Names and Input
 //
 
-let hacker1 = "Manuel";
+let hacker1 = "Hernan";
 console.log(`The driver's name is ${hacker1}`);
 
-let hacker2 = "Victoria";
-console.log(`The driver's name is ${hacker2}`);
+let hacker2 = "Victor";
+console.log(`The navigator's name is ${hacker2}`);
 
 // Iteration 2: Conditionals
 
@@ -26,19 +26,13 @@ let res2 = ""
 
 for (let k = 0; k < hacker2.length-1; k++) {
     res1 += hacker2[k] + " ";
-    console.log(res1);
 }
 res1 += hacker2[hacker2.length -1 ];
 
 
 
 for (let k = hacker1.length - 1; k >= 0; k--) {
-    res2 += hacker1[k];
-    console.log(res2);
-}
-
-console.log(res1.toUpperCase());
-console.log(res2);
+    res2 += hacker1[k];}
 
 k = 0;
 let message= "What?! You both have the same name?";
@@ -49,11 +43,11 @@ let message= "What?! You both have the same name?";
         k++
     }
     else if (hacker1[k] > hacker2[k]) { 
-        message = hacker2 + " The driver's name goes first.";
+        message = hacker1 + " The driver's name goes first.";
         break
 
     } else { 
-        message = hacker1 + " Yo, the navigator goes first definitely";
+        message = hacker2 + " Yo, the navigator goes first definitely";
         break
     }
 
@@ -63,30 +57,89 @@ console.log(message)
 
 // Bonus 1
 
-let paragraphs = `et Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam vel auctor ex, congue mattis velit. Cras faucibus vel lacus ac interdum. Etiam quis leo nulla. Morbi sagittis vitae ligula vitae pretium. Praesent id arcu in felis tincidunt aliquam. Mauris interdum sit amet risus eleifend egestas. Nulla vel odio finibus, varius arcu eu, porta augue. Vivamus consectetur eget elit a tristique. Nulla condimentum pellentesque sem, ut iaculis sapien volutpat sed. Pellentesque non mauris non enim ultricies congue in vel quam.
+let paragraphs = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam vel auctor ex, congue mattis velit. Cras faucibus vel lacus ac interdum. Etiam quis leo nulla. Morbi sagittis vitae ligula vitae pretium. Praesent id arcu in felis tincidunt aliquam. Mauris interdum sit amet risus eleifend egestas. Nulla vel odio finibus, varius arcu eu, porta augue. Vivamus consectetur eget elit a tristique. Nulla condimentum pellentesque sem, ut iaculis sapien volutpat sed. Pellentesque non mauris non enim ultricies congue in vel quam.
 
 Morbi aliquam diam ac mi pulvinar, eget lobortis justo aliquam. Phasellus sodales augue in felis imperdiet mollis. Donec sit amet turpis enim. Mauris ante arcu, malesuada sollicitudin suscipit et, euismod vitae dui. Maecenas vel arcu at turpis egestas viverra quis eget urna. Nullam at pharetra erat. Cras convallis orci velit, sed fermentum sapien luctus at. Vivamus auctor rhoncus nulla, a consequat velit posuere nec. Mauris dignissim est dui, vel tempus ligula rhoncus eu. Vestibulum euismod lacus vitae enim pellentesque dignissim. Duis rutrum sollicitudin nibh, ac varius libero tempus volutpat. Donec vitae eros mattis, pellentesque eros non, bibendum dolor. Donec sed posuere lacus. Nulla a augue justo.
 
 Mauris interdum diam id nibh tincidunt, id volutpat neque ultricies. Sed porttitor velit eget magna ullamcorper convallis. Donec ac imperdiet mi, id gravida nunc. Cras auctor bibendum fermentum. In et eros dapibus, pulvinar tellus laoreet, venenatis nulla. Suspendisse potenti. Etiam et nunc vel orci finibus suscipit. Vivamus ut ullamcorper dolor. Cras interdum varius orci. Suspendisse sit amet aliquam felis, sit amet laoreet tellus. Ut et convallis lorem.`
 
-//on initialise le compteur à 0
-
 paragraphs.toLowerCase();
 
 let count = 0;
-
+   
     for(let k=0 ; k < paragraphs.length; k++) {
-        if (
+        if (  
         (paragraphs[k] === " " && paragraphs[k + 1] === "e" && paragraphs[k + 2] === "t" && paragraphs[k + 3] === " ")
         ||
         (paragraphs[k] === " " && paragraphs[k + 1] === "e" && paragraphs[k + 2] === "t" && paragraphs[k + 3] === "," )
         ||
         (paragraphs[k] === " " && paragraphs[k + 1] === "e" && paragraphs[k + 2] === "t" && paragraphs[k + 3] === ".")
+                ||
+        (paragraphs[k] === " " && paragraphs[k + 1] === "e" && paragraphs[k + 2] === "t" && paragraphs[k + 3] === ".")
+
         )
         {
             count ++;
-            console.log(k / paragraphs.length * 100);
 
-        }    }
+        }    
+    }
+
+    if (paragraphs[0] === "e" && paragraphs[1] === "t" && paragraphs[2] === " " ) {
+        count ++;
+    }
+
 
 console.log(count);
+
+// Bonus 2
+
+let phraseToCheck = "Was it a car or a cat I saw?";
+//let phraseToCheck = "Amor, Roma";
+
+let orderedPhrase = ""
+
+for (let i = 0; i < phraseToCheck.length; i++) {
+    if (
+        (phraseToCheck[i] !== " ") 
+        &&
+        (phraseToCheck[i] !== ",")
+        &&
+        (phraseToCheck[i] !== "?")
+       ) 
+        {
+            orderedPhrase += phraseToCheck[i]
+        }
+}
+
+orderedPhrase = orderedPhrase.toLowerCase();
+
+
+
+let reversedPhrase = "";
+
+for (let i = phraseToCheck.length -1; i >= 0 ; i--) {
+    if (
+        (phraseToCheck[i] !== " ") 
+        &&
+        (phraseToCheck[i] !== ",")
+        &&
+        (phraseToCheck[i] !== "?")
+       ) 
+        {
+            reversedPhrase += phraseToCheck[i];
+        }
+}
+
+reversedPhrase = reversedPhrase.toLowerCase();  
+
+let isPalindrome = true;
+
+for (let i = 0; i < reversedPhrase.length; i++) {
+    if ( reversedPhrase[i] === orderedPhrase[i]) {
+        isPalindrome = true;
+    } else {
+        isPalindrome = false;
+    }
+}
+
+console.log(isPalindrome);
